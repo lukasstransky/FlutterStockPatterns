bfc57aff97b614824c00f5e0334f9641fef4953a
Lukas Stransky <lukas.stransky.at@gmail.com>
2019-09-06 00:08:38 +0200

    implemented sign up functionality with email and password

--- stock_patterns/lib/Pages/Setup/signUp.dart
+++ stock_patterns/lib/Pages/Setup/signUp.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:stock_patterns/Pages/Setup/signIn.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -6,10 +8,62 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       
+      appBar: AppBar(
+        title: Text('Sign up'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (input) {
+                //TODO: check if email is correct
+                if (input.isEmpty) {
+                  return 'Please type an email';
+                }
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (input) {
+                //TODO: check if password is strong enough (if needed)
+                if (input.length < 6) {
+                  return 'Your Password needs to be atleast 6 characters';
+                }
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            RaisedButton(
+              onPressed: signUp,
+              child: Text('Sign up'),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> signUp() async {
+    final formState = _formKey.currentState;
+    if (formState.validate() == true) {
+      formState.save(); //will call onSaved to store email, password
+      try {
+        FirebaseUser user = (await FirebaseAuth.instance
+                .createUserWithEmailAndPassword(email: _email, password: _password))
+            .user;
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+      } catch (e) {
+        print(e.message);
+      }
+    }
   }
 }
