9bbfb7126ae0b432561a199851c88f3b3250fe07
Lukas Stransky <lukas.stransky.at@gmail.com>
2019-09-05 22:12:35 +0200

    added email, password column

--- stock_patterns/lib/Setup/signIn.dart
+++ stock_patterns/lib/Setup/signIn.dart
@@ -6,11 +6,39 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage>{
+
+  String _email, _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign in'),
+      ),
+      body: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (input) { //TODO: check if email is correct
+                if(input.isEmpty) {
+                  return 'Please type an email';
+                }
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (input) { //TODO: check if password is strong enough (if needed)
+                if(input.length < 6) {
+                  return 'Your Password needs to be atleast 6 characters';
+                }
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            )
+          ],
+        ),
       ),
     );
   }
