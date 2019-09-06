25bff6801266df0355edf33a44e970cdd8350926
Lukas Stransky <lukas.stransky.at@gmail.com>
2019-09-06 11:38:13 +0200

    added button to navigate to stock adding page

--- stock_patterns/lib/Pages/home.dart
+++ stock_patterns/lib/Pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:stock_patterns/Pages/addStockAndPatterns.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key, @required this.user}) : super(key: key);
@@ -16,6 +17,16 @@ class _HomeState extends State<Home> {
        appBar: AppBar(
          title: Text('Home ${widget.user.email}'),
        ),
+       body: Form(
+         child: RaisedButton(
+           onPressed: navigateToAddStockAndPatterns,
+           child: Text('add Stock & Patterns'),
+         ),
+       ),
     );
+  }
+
+  void navigateToAddStockAndPatterns(){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => AddStockAndPatterns()));
   }
 }
