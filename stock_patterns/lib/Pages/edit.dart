79d0d9ed9dd32c08776304db2e85dd55109327e2
Lukas Stransky <lukas.stransky.at@gmail.com>
2019-09-07 19:03:52 +0200

    edit page created

---
+++ stock_patterns/lib/Pages/edit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditPage extends StatefulWidget {
+  @override
+  _EditPageState createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title: Text("Edit"),
+       ),
+    );
+  }
+}
